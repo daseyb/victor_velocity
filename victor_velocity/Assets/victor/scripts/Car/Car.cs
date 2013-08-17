e43d60382665b9673aea88d58a9885fce04c76bf
Benjamin Schug <benjamin.schug@gmail.com>
2013-08-17 15:43:17 +0200

    added movable car sprite

---
+++ victor_velocity/Assets/victor/scripts/Car/Car.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace victor.car
+{	
+	public class Car : MonoBehaviour 
+	{
+		public float MaxSteering = 1f;
+	
+		void Update () 
+		{
+			var pos = transform.position;
+			var rel = Input.GetAxis("Horizontal");
+			
+			pos.x = Mathf.Lerp (0, MaxSteering, Mathf.Abs(rel));
+			if (rel < 0) {
+				pos.x = -pos.x;
+			}
+			
+			transform.position = pos;
+		}
+	}
+}
