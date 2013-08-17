7a1f53673d8eb16f6a4d062e0c5620df35e92acd
Dario Seyb <dario.seyb@gmail.com>
2013-08-17 19:57:23 +0200

    -added dashboard animation

---
+++ victor_velocity/Assets/victor/scripts/Dashboard/Display.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Display : MonoBehaviour {
+	GearStick stick;
+	public Texture2D[] gearTextures;
+	// Use this for initialization
+	void Start () {
+		stick = GameObject.Find("GearStick").GetComponent<GearStick>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		renderer.material.mainTexture = gearTextures[stick.Gear];
+	}
+}
